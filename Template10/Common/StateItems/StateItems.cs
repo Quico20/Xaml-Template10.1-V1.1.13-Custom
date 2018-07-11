f75a1d2fcf403b72374c52dfa3d9a1d3c2a8775e
Quico20 <quico20@hotmail.com>
2018-07-11 10:30:06 -0500

    Rename Template10/Common/StateItems.cs to Template10/Common/StateItems/StateItems.cs

---
+++ Template10/Common/StateItems/StateItems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Template10.Common
+{
+    public class StateItems : ObservableDictionary<string, object>, IStateItems
+    {
+        public T Get<T>(string key)
+        {
+            if (TryGetValue(key, out object tryGetValue))
+            {
+                return (T)tryGetValue;
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public bool TryGet<T>(string key, out T value)
+        {
+            bool success = false;
+            if (success = TryGetValue(key, out object tryGetValue))
+            {
+                value = (T)tryGetValue;
+            }
+            else
+            {
+                value = default(T);
+            }
+            return success;
+        }
+    }
+}
