1caa74d92e3da94ee66b8d87308f048aaa51f5e2
Quico20 <quico20@hotmail.com>
2018-07-11 07:29:03 -0500

    Add files via upload

---
+++ Template10/Common/TemplatePartNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Template10.Common
+{
+    public class TemplatePartNotFoundException : Exception
+    {
+        public TemplatePartNotFoundException(string message) : base(message) { }
+    }
+}
