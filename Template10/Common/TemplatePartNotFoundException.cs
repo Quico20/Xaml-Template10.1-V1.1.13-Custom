5fc20d1b09e74608537e6fb2ef362d465fe01359
Quico20 <quico20@hotmail.com>
2018-07-11 10:28:09 -0500

    Rename Template10/Common/TemplatePartNotFoundException.cs to Template10/Common/Exceptions/TemplatePartNotFoundException.cs

--- Template10/Common/TemplatePartNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Template10.Common
-{
-    public class TemplatePartNotFoundException : Exception
-    {
-        public TemplatePartNotFoundException(string message) : base(message) { }
-    }
-}
